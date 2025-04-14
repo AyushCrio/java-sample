50c086494104e3961a1485fb6140121dc3473723
ayush <mfsi.ayushm@gmail.com>
2025-04-14 17:24:01 +0530

    Done my work

---
+++ Simple-Java/src/test/java/Print.java
@@ -0,0 +1,14 @@
+public class Print {
+
+	
+	public static void main(String[] args) 
+	{
+		print();
+	}
+	
+	public static void print()
+	{
+		System.out.println("This the is the first jenking code run");
+	}
+
+}
