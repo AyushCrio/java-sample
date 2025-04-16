8343a953ea3c51e9edde0ef811a3a7f1ce0f0c86
ayush <mfsi.ayushm@gmail.com>
2025-04-16 12:24:06 +0530

    Updated with methods

--- Simple-Java/src/test/java/Print.java
+++ Simple-Java/src/test/java/Print.java
@@ -1,14 +1,29 @@
-public class Print {
+import org.testng.annotations.Test;
 
-	
-	public static void main(String[] args) 
-	{
-		print();
-	}
-	
+public class Print 
+{	
+	@Test
 	public static void print()
 	{
-		System.out.println("This the is the first jenking code run");
+		System.out.println("This method print");
+	}
+	
+	@Test
+	public static void add()
+	{
+		System.out.println("This method add 5 + 5 = " + (5+5));
+	}
+	
+	@Test
+	public static void sub()
+	{
+		System.out.println("This method subtract 10 - 5 = " + (10-5));
+	}
+	
+	@Test
+	public static void mul()
+	{
+		System.out.println("This method multiply 7 * 5 = " + (7*5));
 	}
 
 }
